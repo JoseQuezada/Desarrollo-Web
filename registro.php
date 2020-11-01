eaa48553823da2a3bcd3da9250190af93379c31b
JoseQuezada <josequezada.lh.est@outlook.es>
2020-10-31 18:31:11 -0600

    Redirigir al registro en agregar usuario

--- registro.php
+++ registro.php
@@ -32,8 +32,8 @@ require_once './php/crear_usuario.php';
 		<div id="signupbox" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
 			<div class="panel panel-info">
 				<div class="panel-heading">
-					<div class="panel-title">Reg&iacute;strate</div>
-					<div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="index.php">Iniciar Sesi&oacute;n</a></div>
+					<div class="panel-title">Registro de Usuarios</div>
+					<!--<div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="index.php">Iniciar Sesi&oacute;n</a></div> -->
 				</div>
 
 				<div class="panel-body">
