5e706d5b9887df4febc7b1d3dfe318a34c7d11f4
JoseQuezada <josequezada.lh.est@outlook.es>
2020-11-03 21:25:26 -0600

    botones actualizar y cancelar

--- ActualizarProveedor.php
+++ ActualizarProveedor.php
@@ -101,7 +101,8 @@
 
                     <div class="form-group">
                         <div class="col-md-offset-5 col-md-9">
-                            <button id="btn-signup" type="submit" class="btn btn-info"><i class="icon-hand-right"></i>Registrar Proveedor</button>
+                        <button id="btn-signup" type="submit" class="btn btn-sm btn-primary"><i class="icon-hand-right"></i>Actualizar</button>
+                                            <button id="btn-signup" type="submit" class="btn btn-sm btn-danger"><i class="icon-hand-right"></i>Cancelar</button>
                         </div>
                     </div>
                 </form>
