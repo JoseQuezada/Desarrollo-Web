f5a81bfd82cab11a30a39c748bb1c88846d097a2
JoseQuezada <josequezada.lh.est@outlook.es>
2020-11-04 01:24:12 -0600

    Quitar registrate

--- recupera.php
+++ recupera.php
@@ -25,7 +25,7 @@ require './php/recuperarPassword.php';
 		<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
 			<div class="panel panel-info">
 				<div class="panel-heading">
-					<div class="panel-title">Recuperar Password</div>
+					<div class="panel-title">Recuperar Contraseña</div>
 					<div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="index.php">Iniciar Sesi&oacute;n</a></div>
 				</div>
 
@@ -63,9 +63,7 @@ require './php/recuperarPassword.php';
 
 						<div class="form-group">
 							<div class="col-md-12 control">
-								<div style="border-top: 1px solid#888; padding-top:15px; font-size:85%">
-									No tiene una cuenta! <a href="registro.php">Registrate aquí</a>
-								</div>
+								
 							</div>
 						</div>
 					</form>
