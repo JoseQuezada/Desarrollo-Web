e6518862a1d6dba309d600690a67686f9152dc06
Pipa <damianpeaf@gmail.com>
2020-11-05 02:04:27 -0600

    terminando el detalle de la compra y unos errores

--- detalleCompra.php
+++ detalleCompra.php
@@ -28,48 +28,48 @@ if ($IDCompra != null) {
 }
 
 
-require './php/Compra.php';
+// require './php/Compra.php';
 
-$error = false;
+// $error = false;
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') { //Chequea si se accedió por medio de POST
+// if ($_SERVER['REQUEST_METHOD'] == 'POST') { //Chequea si se accedió por medio de POST
 
-    $fecha = $_POST["fecha"];
-    $descripcion = $_POST["desripcion"];
+//     $fecha = $_POST["fecha"];
+//     $descripcion = $_POST["desripcion"];
 
-    $idInsumos = $_POST["idInsumos"];
-    $librasCompradas = $_POST["librasCompradas"];
-    $idDetalle = $_POST["idDetalle"];
+//     $idInsumos = $_POST["idInsumos"];
+//     $librasCompradas = $_POST["librasCompradas"];
+//     $idDetalle = $_POST["idDetalle"];
 
-    $largoDatos = 0; 
+//     $largoDatos = 0; 
 
-    if (count($idInsumos) == count($librasCompradas)) {
-        $largoDatos = count($idInsumos);
-    }else{
-        echo "<script>alert('Hubo un error');</script>";
-        exit();
-    }
+//     if (count($idInsumos) == count($librasCompradas)) {
+//         $largoDatos = count($idInsumos);
+//     }else{
+//         echo "<script>alert('Hubo un error');</script>";
+//         exit();
+//     }
 
-    $infoInsumos = [];
+//     $infoInsumos = [];
 
-    for ($i = 0; $i < $largoDatos; $i++) {
-        $infoInsumos[] = [
-            $idInsumos[$i] => $librasCompradas[$i]
-        ];
-    }
+//     for ($i = 0; $i < $largoDatos; $i++) {
+//         $infoInsumos[] = [
+//             $idInsumos[$i] => $librasCompradas[$i]
+//         ];
+//     }
 
-    echo var_dump($infoInsumos);
+//     echo var_dump($infoInsumos);
 
-    $compra = new Compra();
-    // $error = $compra->crearCompra($fecha, $descripcion, $infoInsumos, $idDetalle);
+//     $compra = new Compra();
+//     // $error = $compra->crearCompra($fecha, $descripcion, $infoInsumos, $idDetalle);
 
-    // $error = $proveedor -> crearProveedor($empresa, $nombre, $apellidos, $direccion, $telefono, $email);
+//     // $error = $proveedor -> crearProveedor($empresa, $nombre, $apellidos, $direccion, $telefono, $email);
 
-    if(!$error){
-        echo "<script>alert('Ingresado correctamente');</script>";
-    }
+//     if(!$error){
+//         echo "<script>alert('Ingresado correctamente');</script>";
+//     }
 
-
+// }
 
 
 ?>
